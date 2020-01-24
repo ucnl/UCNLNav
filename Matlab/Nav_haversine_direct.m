cc55648b7ac6a48094763d4a022e4d3887b946c9
Aleksandr <aleksandr.v.dikarev@gmail.com>
2020-01-24 11:02:34 +0400

    Update Nav_haversine_direct.m

--- Matlab/Nav_haversine_direct.m
+++ Matlab/Nav_haversine_direct.m
@@ -5,5 +5,5 @@ function [ ep_lat_rad, ep_lon_rad ] = Nav_haversine_direct(sp_lat_rad, sp_lon_ra
     ep_lat_rad = Nav_wrap_2pi(asin(sin(sp_lat_rad) * cos(delta) + cos(sp_lat_rad) * sin(delta) * cos(fwd_az_rad)));
     
     ep_lon_rad = Nav_wrap_2pi(3 * pi +... 
-        (sp_lon_rad + atan2(sin(fwd_az_rad) * sin(delta) * cos(sp_lat_rad), (cos(delta) - sin(sp_lat_rad)^2)))) - pi;
+        (sp_lon_rad + atan2(sin(fwd_az_rad) * sin(delta) * cos(sp_lat_rad), (cos(delta) - sin(sp_lat_rad) * sin(ep_lat_rad))))) - pi;
 end
