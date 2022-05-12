c5e492bb475554fb58c18ba90910f1503b66d4df
Aleksandr <aleksandr.v.dikarev@gmail.com>
2022-05-13 00:35:25 +0300

    Update Nav_build_ellipsoid.m

--- Matlab/Nav_build_ellipsoid.m
+++ Matlab/Nav_build_ellipsoid.m
@@ -12,13 +12,14 @@ end
 
 fltn = 1 / ifltn;
 mnsa_m = mjsa_m * (1 - fltn);
-ecnt = (mjsa_m^2 - mnsa_m^2) / mjsa_m^2;
+ecnt_sq = (mjsa_m^2 - mnsa_m^2) / mjsa_m^2;
+ecnt = sqrt(ecnt_sq);
 
 result.mjsa_m = mjsa_m;
 result.ifltn = ifltn;
 result.fltn = fltn;
 result.mnsa_m = mnsa_m;
 result.ecnt = ecnt;
-result.ecnt_sq = ecnt^2;
+result.ecnt_sq = ecnt_sq;
 
 end
